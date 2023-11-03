29141cb6b67118eeff908cb734f74893a84cf666
Artur99M <142691984+Artur99M@users.noreply.github.com>
2023-11-03 14:46:57 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -13,8 +13,8 @@ struct stack {
 
 int isFull (stack* p);
 int isEmpty (stack* p);
-int StackPush(stack* p, const elem_t number);
-elem_t StackPop(stack* p, int* ERROR);
+int StackPush (stack* p, const elem_t number);
+elem_t StackPop (stack* p, int* ERROR);
 int StackCtor (stack* p, const size_t capacity);
 int StackDtor (stack* p);
 
