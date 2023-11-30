c918e5963498ee3e7a25be94594b4e86f4c364bf
Artur99M <142691984+Artur99M@users.noreply.github.com>
2023-11-30 17:39:37 +0300

    Update stack_protection.h

--- stack_protection.h
+++ stack_protection.h
@@ -1,10 +1,10 @@
 #ifndef STACK_PROTECTION_H
 #define STACK_PROTECTION_H
 
-#include "stack.h"
-
 #define STACK_DUMP(p) StackDump(p, #p, __FILE__, __LINE__, __func__)
 
-void StackDump (stack p, char* stackname, char* file, int numline, const char* func);
+#include "stack.h"
+
+void StackDump (struct stack p, char* stackname, char* file, int numline, const char* func);
 
 #endif
