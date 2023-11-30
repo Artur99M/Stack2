f9182db39ae21b52d335914ae246ffc044702ba6
Artur99M <142691984+Artur99M@users.noreply.github.com>
2023-11-30 17:39:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include "stack.h"
 #include "stack_protection.h"
 
-int main (int argc, char* argv [])
+int main ()
 {
     stack p;
     StackCtor (&p, 5);
 
     for (int i = 0; i < 100; i++)
-        StackPush (&p, i);
-
+        printf ("%d %d\n", i, StackPush (&p, i));
+    putchar ('\n');
     int q = 0;
     for (int i = 0; i < 60; i++)
         printf ("%d %d\n", StackPop (&p, &q), q);
+
 
     STACK_DUMP (p);
     StackDtor (&p);
